84c69b201f3904429bac39b5a6542c912bcf578c
Raghav2226219 <124338240+Raghav2226219@users.noreply.github.com>
2025-02-14 07:52:29 +0000

    data conversion done

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,16 @@
+let score = 90;
+let score2 = "45";
+
+console.log(typeof score);
+
+console.log(typeof score2);
+
+let valueInNumber = Number(score2); // it convert string to number
+
+console.log(typeof valueInNumber);
+
+// "87" => 87;
+// "45sgs" => NaN;
+// true => 1;
+// false => 0;
+// null => 0;
