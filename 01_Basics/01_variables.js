905eef1dadcf9b35c25efa68423a13a790ffc0ae
Raghav2226219 <124338240+Raghav2226219@users.noreply.github.com>
2025-02-14 07:39:05 +0000

    variables and datatypes done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accId = 1445538;
+let accEmail = "abc@gmail.com";
+var accPassword = "23456";
+accCity = "Patiala"; // try avoiding this 
+// avoid using var prefer using let and const
+// accId = 6676;  // not allowed
+accEmail = "abcdef@gmail.com";
+accPassword = "23456"; 
+accCity = "Karnal";
+let accState;
+
+console.log(accId);
+console.table([accId,accEmail,accPassword,accCity,accState]);
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("yo!! Welcome Brooo"); //using node  js not browser
+
+// datatypes
+// number => 2^53
+// bifint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+console.log(typeof "Raghav")
