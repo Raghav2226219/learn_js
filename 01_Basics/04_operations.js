6a442246fa8c4172fc0f28c2141f9b5c5e9c18bd
Raghav2226219 <124338240+Raghav2226219@users.noreply.github.com>
2025-02-15 10:42:01 +0000

    operater and comparison done

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,20 @@
+let val = 3;
+let neg = -val;
+
+// console.log(neg); // output: -3
+
+let str1 = "hello ";
+let str2 = "Raghav";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+// console.log(+true); // output: 1
+// console.log(+""); // output: 0
+
---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,12 @@
+// console.log("2" > 1); // automatically converted into number hence give true
+
+// avoid these conversions
+
+// console.log(null > 0);
+// console.log(null == 0); 
+// console.log(null < 0); 
+// console.log(null >= 0); 
+
+console.log("2" == 2);
+console.log("2" === 2); // check datatype
+
