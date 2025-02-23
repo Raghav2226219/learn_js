4616c9867ffe0dc03b8182f298e595ff8f07152d
Raghav2226219 <124338240+Raghav2226219@users.noreply.github.com>
2025-02-23 05:58:22 +0000

    Date time done [just start]

---
+++ 01_Basics/08_dates_and_time.js
@@ -0,0 +1,13 @@
+// Dates
+
+let dat = new Date();
+console.log(dat.toString());
+console.log(dat.toDateString());
+console.log(dat.toISOString());
+console.log(dat.toJSON());
+console.log(dat.toLocaleDateString());
+
+// Time
+
+let tim = Date.now();
+console.log(tim);
