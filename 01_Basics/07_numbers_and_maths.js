199cfccf9355a0f94d64144ee72cbf36d23f6b7c
Raghav2226219 <124338240+Raghav2226219@users.noreply.github.com>
2025-02-23 05:37:02 +0000

    numbers and math done

---
+++ 01_Basics/07_numbers_and_maths.js
@@ -0,0 +1,25 @@
+// const score = 500;
+// console.log(score);
+
+// const bal = new Number(1000);
+// console.log(bal);
+
+// console.log(bal.toString());
+// console.log(bal.toFixed(2));
+
+// const num = 23.5968;
+// console.log(num.toPrecision(3));
+
+// const num2 = 100000000;
+// console.log(num2.toLocaleString());
+// console.log(num2.toLocaleString('en-In'));
+
+
+// ##################### MATHS ########################
+
+console.log(Math.abs(-9));
+console.log(Math.round(5.66));
+console.log(Math.abs(-9));
+console.log(Math.ceil(5.66));
+console.log(Math.floor(5.66));
+console.log(Math.random());
