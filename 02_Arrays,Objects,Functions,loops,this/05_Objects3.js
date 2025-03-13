a58c05987a4101bc9dd8d522bc5acea85c3e8366
Raghav2226219 <124338240+Raghav2226219@users.noreply.github.com>
2025-03-13 11:59:43 +0000

    Objects done

---
+++ 02_Arrays,Objects,Functions,loops,this/05_Objects3.js
@@ -0,0 +1,17 @@
+const course = {
+    coursename: "Javascript",
+    price: "599",
+    instructor: "Sam"
+}
+
+console.log(course.instructor);
+
+const {instructor} = course // destructuring in js mainly used in react
+console.log(instructor);
+
+// {
+//     "name": "cap",
+//     "cn": "ddd",
+//     "price": "free"
+// } json format
+
