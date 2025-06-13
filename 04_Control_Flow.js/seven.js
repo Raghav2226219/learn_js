75cc6cb8ca4fccfc4233e106c4706d7d14cf9121
Raghav2226219 <124338240+Raghav2226219@users.noreply.github.com>
2025-06-13 11:27:13 +0000

    DOM started queryselector done along with get Element

--- 04_Control_Flow.js/seven.js
+++ 04_Control_Flow.js/seven.js
@@ -2,6 +2,7 @@ const nums = [1,2,3,4,5,6,7,8,9,10]
 
 // const nn = nums.map((num) => num + 10);
 
+// below given is chaining i.e, map is applied after map and further followed by filter 
 const nn = nums.map((num) => num * 10).map((num) => num*10).filter((num) => num > 400);
 
 console.log(nn);
